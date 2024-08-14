557b4fa01c44fc10a518f079b030b5361ef605d0
Jake Rachleff <jake@langchain.dev>
2024-08-13 17:48:40 -0700

    remove reviser

--- document_summarizer_few_shot/agent.py
+++ document_summarizer_few_shot/agent.py
@@ -153,132 +153,6 @@ def write_tweet(state, config):
     attempts = state.get("attempts", 0) or 0
     return {"tweet": tweet, "attempts": attempts + 1}
 
-# TODO: REPLACE WITH FEW SHOT SEARCH
-def get_harrision_tweets_examples():
-    return  """
-        <example>
-        It's out! LangChain v0.1.0 comes out with an improved package architecture for stability and production readiness, as well a focus on:
-        👀 Observability
-        ↔️ Integrations
-        🔗 Composability
-        🏳️ Streaming
-        🧱 Output Parsing
-        🔍 Retrieval
-        🤖 Agents
-        </example>
-
-        <example>
-        this is a really cool project - its agent that writes other agents
-        </example>
-
-        <example>
-        This was a nights and weekend project for me, but I had a lot of fun making it and think there's some good opportunities to improve it
-        See a walkthrough here: youtu.be/OM6ibrjn_Sg
-        </example>
-
-        <example>
-        🪖 LangGraph Engineer
-        This is an alpha version of an agent that can help bootstrap LangGraph applications
-        It will focus on creating the correct nodes and edges, but will not attempt to write the logic to fill in the nodes and edges - rather will leave that for you
-        Try out the deployed version: smith.langchain.com/studio/thread?…
-        The agent consists of a few steps:
-
-        1. Converse with the user to gather all requirements
-        2. Write a draft
-        3. Run programmatic checks against the generated draft (right now just checking that the response has the right format). If it fails, then go back to step 2. If it passes, then continue to step 4.
-        4. Run an LLM critique against the generated draft. If it fails, go back to step 2. If it passes, the continue to the end.
-        
-        Deployed on LangGraph Cloud, and made publicly accessible (the ability to do this is behind a feature flag, DM me if interested)
-        Code: github.com/hwchase17/lang…
-        </example>
-
-        <example>
-        Opening up access for LangGraph Cloud!
-        </example>
-
-        <example>
-        Once of the best things about LangGraph is the built in persistence layer
-        This enables all sorts of human-in-the-loop interactions
-        We've released LangGraph 0.2 which improves management of that and open-sourced our Postgres implementation
-        </example>
-    """
-
-def harrison_critique(state, config):
-    print("sounds like state: ", state)
-
-    tweet = state.get("tweet")
-    if not tweet:
-        raise ValueError("No tweet provided")
-
-    sounds_like_harrison = ChatOpenAI().with_structured_output(SoundsLikeHarrison).invoke(
-        [
-            {
-                "role": "system",
-                "content": f"""
-                    You are a helpful assistant. Here are past examples of tweets from Harrison Chase:
-                    {get_harrision_tweets_examples()}
-
-                    Harrison tweets highlighting information related to LangChain, his LLM company, and AI in general.
-                    He uses emojis. He uses exclamation points but is not overly enthusiastic. He never uses hashtags.
-                    He sometimes make spelling mistakes. He is not overly formal. He is not "salesy". He is nice. He is concise.
-                 """
-            },
-            {
-                "role": "human",
-                "content": "Does the following tweet sound like Harrison Chase: " + tweet
-            }
-        ]
-    )
-
-    return {"critique": sounds_like_harrison}
-def sounds_like_harrison_checker(state, config):
-    attempts = state.get("attempts", 0)
-
-    if attempts > 4:
-        return "done"
-    sounds_like_harrison = state['critique'].sounds_like_harrison
-    if not sounds_like_harrison:
-        return "revise"
-    else:
-        return "done"
-
-
-reviser_human_prompt = """Revise the following tweet to be more in the style of Harrison Chase's past tweets.
-
-Here is some direct feedback: {feedback}
-
-Here's the tweet:
-
-{tweet}"""
-
-def harrison_tweet_reviser(state, config):
-    print("reviser state: ", state)
-    tweet = state.get("tweet")
-    if not tweet: 
-        raise ValueError("No tweet provided")
-    
-    revised_tweet = ChatOpenAI().invoke(
-        [
-            {
-                "role": "system", 
-                "content": f"""
-                You are a helpful assistant. 
-
-                Here are past examples of tweets from Harrison Chase:
-                {get_harrision_tweets_examples()}
-             """
-            },
-            {
-                "role": "human", 
-                "content": reviser_human_prompt.format(tweet=tweet, feedback=state['critique'].critique)
-            }
-        ]
-    ).content
-
-    curr_attempts = state.get("attempts", 0)
-    print(f"curr_attempts: {curr_attempts}")
-    return {"tweet": revised_tweet, "attempts": curr_attempts + 1}
-
 
 def _check_if_enough_info(state: DirectSummarizerState) -> Literal["write_tweet", END]:
     if "facts" in state:
@@ -290,25 +164,12 @@ def _check_if_enough_info(state: DirectSummarizerState) -> Literal["write_tweet"
 summarizer_workflow = StateGraph(DirectSummarizerState, input=GraphInput, output=GraphOutput)
 summarizer_workflow.add_node(get_contents)
 summarizer_workflow.add_node(convert_to_facts)
-summarizer_workflow.add_node(harrison_critique)
 summarizer_workflow.add_node(write_tweet)
-summarizer_workflow.add_node(harrison_tweet_reviser)
 
 summarizer_workflow.set_entry_point("get_contents")
 summarizer_workflow.add_edge("get_contents", "convert_to_facts")
-summarizer_workflow.add_edge("write_tweet", "harrison_critique")
-summarizer_workflow.add_edge("harrison_tweet_reviser", "harrison_critique")
 summarizer_workflow.add_conditional_edges("convert_to_facts", _check_if_enough_info)
-
-
-summarizer_workflow.add_conditional_edges(
-    "harrison_critique",
-    sounds_like_harrison_checker, 
-    {
-        "revise": "harrison_tweet_reviser",
-        "done": END
-    }
-)
+summarizer_workflow.add_edge("write_tweet", END)
 
 
 summarizer_graph = summarizer_workflow.compile()
