67a70a7e29324cd4b194b102cf36764d8c2ff5c4
Harrison Chase <hw.chase.17@gmail.com>
2024-08-18 17:26:18 -0700

    cr

---
+++ app.py
@@ -0,0 +1,66 @@
+import streamlit as st
+import requests
+from langgraph_sdk import get_client
+from functools import partial
+import asyncio
+from langsmith import Client
+
+
+ls_client = Client()
+
+
+
+async def _predict(url):
+    client = get_client(url="http://localhost:51636")
+    thread = await client.threads.create()
+    value = None
+    async for chunk in client.runs.stream(
+            thread["thread_id"],
+            "tweeter",
+            input={"url": url},
+            stream_mode="values"
+        ):
+        if chunk.event=="values":
+            value = chunk.data.get("tweet")
+            content = chunk.data.get("content")
+    return (value, content)
+
+def generate_tweet(url):
+    async_func = partial(_predict, *[url], )
+    loop = asyncio.new_event_loop()
+    contents = loop.run_until_complete(async_func())
+    return contents
+
+def call_api(tweet, context):
+    ls_client.create_examples(
+        inputs=[{"content": context}],
+        outputs=[{"tweet": tweet}],
+        dataset_id="f6320c32-a6ff-46f1-9cc4-1b65e3e14a07",
+    )
+
+st.title("Tweet Generator")
+
+url = st.text_input("Enter a URL:")
+
+if url:
+    if 'generated_tweet' not in st.session_state:
+        t, c = generate_tweet(url)
+        print(c)
+        st.session_state.generated_tweet = t
+        st.session_state.context = c
+
+    tweet = st.text_area(
+        "Generated Tweet:",
+        value=st.session_state.generated_tweet,
+        height=300,
+        key="tweet_area"
+    )
+
+    if st.button("Copy and Send"):
+        # Get the current content of the text area
+        current_tweet = st.session_state.tweet_area
+        # Send to API
+        call_api(current_tweet, st.session_state.context)
+
+st.markdown("---")
+st.markdown("Enter a URL above to generate a tweet. You can edit the generated tweet before copying and sending.")
--- document_summarizer_few_shot/agent.py
+++ document_summarizer_few_shot/agent.py
@@ -6,6 +6,7 @@ from langchain_core.pydantic_v1 import BaseModel, Field
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 from langchain_openai.chat_models import ChatOpenAI
+from langchain_anthropic.chat_models import ChatAnthropic
 
 from langchain_openai import ChatOpenAI
 from langgraph.graph import StateGraph, END
@@ -88,6 +89,7 @@ def convert_to_facts(state, config):
 base_prompt = """You are Harrison Chase. You tweet highlighting information related to LangChain, your LLM company.
 You use emojis. You use exclamation points but are not overly enthusiastic. You never use hashtags.
 You sometimes make spelling mistakes. You are not overly formal. You are not "salesy". You are nice.
+Don't be too cheesy.
 
 You summarize lists of facts derived from articles into tweets. Here are past examples:
 """
@@ -122,6 +124,7 @@ def write_tweet(state, config):
     messages.append(HumanMessage(content="".join(["<fact> " + fact + "</fact>" for fact in facts])))
 
     chain = ChatOpenAI() | StrOutputParser()
+    chain = ChatAnthropic(temperature=0, model_name="claude-3-5-sonnet-20240620") | StrOutputParser()
     
     tweet = chain.invoke(messages)
 
@@ -149,20 +152,68 @@ summarizer_workflow.add_edge("write_tweet", END)
 
 summarizer_graph = summarizer_workflow.compile()
 
+class DirectSummarizerState(TypedDict):
+    url: str
+    content: str
+    tweet: str
+
+
+base_prompt_article = """You are Harrison Chase. You tweet highlighting information related to LangChain, your LLM company.
+You use emojis. You use exclamation points but are not overly enthusiastic. You never use hashtags.
+You sometimes make spelling mistakes. You are not overly formal. You are not "salesy". You are nice.
+
+When given an article, write a tweet about it. Make it relevant and specific to the article at hand.
+
+Pay attention to the examples below. These are good examples. Generate future tweets in the style of the tweets below."""
+def write_tweet_from_article(state, config):
+    print("tweets state: ", state)
+
+    from langsmith import traceable, wrappers
+    from openai import Client
+    from langsmith import traceable, wrappers
+
+    examples = search_similar_examples(
+        dataset_id="f6320c32-a6ff-46f1-9cc4-1b65e3e14a07",
+        inputs_dict={
+            "content": state['content']
+        },
+        limit=5
+    )["examples"]
+
+    messages = [SystemMessage(content=base_prompt_article)]
+    for e in examples:
+        messages.append(HumanMessage(content=e['inputs']['content']))
+        messages.append(AIMessage(content=e['outputs']['tweet']))
+
+    messages.append(HumanMessage(content=state['content']))
+
+    chain = ChatOpenAI(model_name="gpt-4o") | StrOutputParser()
+
+    tweet = chain.invoke(messages)
+
+    attempts = state.get("attempts", 0) or 0
+    return {"tweet": tweet, "attempts": attempts + 1}
 
 tweet_workflow = StateGraph(DirectSummarizerState, input=GraphInput, output=GraphOutput)
 tweet_workflow.add_node(get_contents)
-tweet_workflow.add_node(convert_to_facts)
-tweet_workflow.add_node(write_tweet)
+tweet_workflow.add_node(write_tweet_from_article)
 
 tweet_workflow.set_entry_point("get_contents")
-tweet_workflow.add_edge("get_contents", "convert_to_facts")
-tweet_workflow.add_conditional_edges("convert_to_facts", _check_if_enough_info)
-tweet_workflow.add_edge("write_tweet", END)
+tweet_workflow.add_edge("get_contents", "write_tweet_from_article")
+tweet_workflow.add_edge("write_tweet_from_article", END)
 
 
 tweet_graph = tweet_workflow.compile()
 
 
+# this is the graph making function that will decide which graph to
+# build based on the provided config
+def make_graph(config):
+    raw_article = config.get("raw_article", {}).get("raw_article", True)
+    # route to different graph state / structure based on the user ID
+    if raw_article:
+        return tweet_graph
+    else:
+        return summarizer_graph
 
 
